b878bee6e94cb6ebe14ac2568466fba844e26443
Álvaro García <alvaro@binarynonsense.com>
2023-06-09 14:09:23 +0200

    Add extract tool

---
+++ src/extract.js
@@ -0,0 +1,337 @@
+const fs = require("fs");
+const path = require("path");
+const FileType = require("file-type");
+const sharp = require("sharp");
+
+const fileFormats = require("./file-formats");
+const fileUtils = require("./file-utils");
+const { FileExtension, FileDataType } = require("./constants");
+
+async function start(inputFilePaths, options, command) {
+  /////////////////////////////////
+  // validate input
+  /////////////////////////////////
+  // arguments
+  let filePaths = [];
+  inputFilePaths.forEach((inputFilePath) => {
+    if (fs.existsSync(inputFilePath)) {
+      try {
+        let stats = fs.statSync(inputFilePath);
+        if (stats.isFile()) {
+          if (fileFormats.hasBookExtension(inputFilePath)) {
+            // make absolute
+            inputFilePath = path.resolve(inputFilePath);
+            // avoid duplicate paths
+            if (filePaths.indexOf(inputFilePath) === -1)
+              filePaths.push(inputFilePath);
+          }
+        } else if (stats.isDirectory()) {
+          // TODO: get files from dir? recursive??
+          // Ignore for now
+        }
+      } catch (error) {}
+    }
+  });
+  if (filePaths.length <= 0) {
+    console.log("No comic book files found");
+    return;
+  }
+  // options
+  let outputFolderPath;
+  try {
+    outputFolderPath = path.resolve(options.outputFolder);
+    if (fs.existsSync(outputFolderPath)) {
+      try {
+        let stats = fs.statSync(outputFolderPath);
+        if (!stats.isDirectory()) {
+          throw "The path didn't correspond to a folder";
+        }
+      } catch (error) {
+        // shouldn't be able to reach here
+        throw "The folder didn't exist";
+      }
+    } else {
+      if (options.createOutputFolder === "true") {
+        fs.mkdirSync(outputFolderPath, { recursive: true });
+        console.log("Output folder created: " + outputFolderPath);
+      } else {
+        throw "The folder didn't exist and --create-output-folder was set to false";
+      }
+    }
+  } catch (error) {
+    console.log("Error: the output folder's path is invalid > " + error);
+    return;
+  }
+  let outputScale = parseInt(options.scale);
+  if (!Number.isInteger(outputScale) || outputScale <= 0 || outputScale > 100) {
+    outputScale = 100;
+  }
+  let imageOutputFormat = options.imageFormat;
+  if (imageOutputFormat === "original")
+    imageOutputFormat = FileExtension.NOT_SET;
+  let imageOutputQuality = parseInt(options.imageQuality);
+  if (
+    !Number.isInteger(imageOutputQuality) ||
+    imageOutputQuality <= 0 ||
+    imageOutputQuality > 100
+  ) {
+    imageOutputQuality = 90;
+  }
+  let pdfExtractionMethod = options.pdfExtractionMethod;
+  /////////////////////////////////
+  // extracting files
+  /////////////////////////////////
+  console.log(`\Extracting ${filePaths.length} file/s`);
+  let summaryTotal = filePaths.length;
+  let summarySuccesses = 0;
+  let summaryErrors = 0;
+  for (let index = 0; index < filePaths.length; index++) {
+    const filePath = filePaths[index];
+    let fileName = path.basename(filePath, path.extname(filePath));
+    let outputSubFolderPath = path.join(outputFolderPath, fileName);
+    let i = 1;
+    while (fs.existsSync(outputSubFolderPath)) {
+      i++;
+      outputSubFolderPath = path.join(
+        outputFolderPath,
+        fileName + "(" + i + ")"
+      );
+    }
+    console.log("");
+    console.log("Extracting " + filePath);
+    try {
+      /////////////////////////////////
+      // get file type
+      /////////////////////////////////
+      let fileType;
+      let fileExtension = path.extname(filePath).toLowerCase();
+      let _fileType = await FileType.fromFile(filePath);
+      if (_fileType !== undefined) {
+        fileExtension = "." + _fileType.ext;
+      }
+      if (fileExtension === "." + FileExtension.PDF) {
+        fileType = FileDataType.PDF;
+      } else if (fileExtension === "." + FileExtension.EPUB) {
+        fileType = FileDataType.EPUB;
+      } else {
+        if (
+          fileExtension === "." + FileExtension.RAR ||
+          fileExtension === "." + FileExtension.CBR
+        ) {
+          fileType = FileDataType.RAR;
+        } else if (
+          fileExtension === "." + FileExtension.ZIP ||
+          fileExtension === "." + FileExtension.CBZ
+        ) {
+          fileType = FileDataType.ZIP;
+        } else if (
+          fileExtension === "." + FileExtension.SEVENZIP ||
+          fileExtension === "." + FileExtension.CB7
+        ) {
+          fileType = FileDataType.SEVENZIP;
+        } else {
+          throw "The file is not of a valid of type";
+        }
+      }
+      /////////////////////////////////
+      // extract content
+      /////////////////////////////////
+      let tempFolderPath = fileUtils.createTempFolder();
+      console.log("\textracting images...");
+      try {
+        switch (fileType) {
+          case FileDataType.RAR:
+            await fileFormats.extractRar(filePath, tempFolderPath);
+            break;
+          case FileDataType.ZIP:
+            fileFormats.extractZip(filePath, tempFolderPath);
+            break;
+          case FileDataType.SEVENZIP:
+            await fileFormats.extract7Zip(filePath, tempFolderPath);
+            break;
+          case FileDataType.EPUB:
+            await fileFormats.extractEpub(filePath, tempFolderPath);
+            break;
+          case FileDataType.PDF:
+            await fileFormats.extractPdf(
+              filePath,
+              tempFolderPath,
+              pdfExtractionMethod
+            );
+            break;
+        }
+      } catch (error) {
+        throw "Couldn't extract the pages > " + error;
+      }
+      /////////////////////////////////
+      // modify content
+      /////////////////////////////////
+      let comicInfoFilePath =
+        fileUtils.getComicInfoFileInFolderRecursive(tempFolderPath);
+      let imgFilePaths =
+        fileUtils.getImageFilesInFolderRecursive(tempFolderPath);
+      if (imgFilePaths === undefined || imgFilePaths.length === 0) {
+        throw "No images could be found";
+      }
+      imgFilePaths.sort(fileUtils.compare);
+      // resize ////////////////////////
+      let didResize = false;
+      if (outputScale < 100) {
+        try {
+          didResize = true;
+          console.log("\tresizing images...");
+          sharp.cache(false);
+          for (let index = 0; index < imgFilePaths.length; index++) {
+            console.log(
+              "\tresizing image " +
+                ": " +
+                (index + 1) +
+                " / " +
+                imgFilePaths.length
+            );
+            let imgFilePath = imgFilePaths[index];
+            let imgFileFolderPath = path.dirname(imgFilePath);
+            let imgFileName = path.basename(
+              imgFilePath,
+              path.extname(imgFilePath)
+            );
+            let tmpImgFilePath = path.join(
+              imgFileFolderPath,
+              imgFileName + "." + FileExtension.TMP
+            );
+            let data = await sharp(imgFilePath).metadata();
+            await sharp(imgFilePath)
+              .withMetadata()
+              .resize(Math.round(data.width * (outputScale / 100)))
+              .toFile(tmpImgFilePath);
+
+            fs.unlinkSync(imgFilePath);
+            fileUtils.moveFile(tmpImgFilePath, imgFilePath);
+          }
+        } catch (error) {
+          throw "Couldnt resize the images > " + error;
+        }
+      }
+      // reformat /////////////////////
+      // change image format if requested
+      let didChangeFormat = false;
+      if (imageOutputFormat !== FileExtension.NOT_SET) {
+        console.log("\tconverting images...");
+        try {
+          sharp.cache(false); // avoid EBUSY error on windows
+          for (let index = 0; index < imgFilePaths.length; index++) {
+            let imgFilePath = imgFilePaths[index];
+            let imgFileFolderPath = path.dirname(imgFilePath);
+            let imgFileName = path.basename(
+              imgFilePath,
+              path.extname(imgFilePath)
+            );
+            if (imageOutputFormat != FileExtension.NOT_SET) {
+              didChangeFormat = true;
+              console.log(
+                "\tconverting image " +
+                  ": " +
+                  (index + 1) +
+                  " / " +
+                  imgFilePaths.length
+              );
+              let imgTmpFilePath = path.join(
+                imgFileFolderPath,
+                imgFileName + "." + FileExtension.TMP
+              );
+              if (imageOutputFormat === FileExtension.JPG) {
+                await sharp(imgFilePath)
+                  .withMetadata()
+                  .jpeg({
+                    quality: imageOutputQuality,
+                  })
+                  .toFile(imgTmpFilePath);
+              } else if (imageOutputFormat === FileExtension.PNG) {
+                if (imageOutputQuality < 100) {
+                  await sharp(imgFilePath)
+                    .withMetadata()
+                    .png({
+                      quality: imageOutputQuality,
+                    })
+                    .toFile(imgTmpFilePath);
+                } else {
+                  await sharp(imgFilePath)
+                    .withMetadata()
+                    .png()
+                    .toFile(imgTmpFilePath);
+                }
+              } else if (imageOutputFormat === FileExtension.WEBP) {
+                await sharp(imgFilePath)
+                  .withMetadata()
+                  .webp({
+                    quality: imageOutputQuality,
+                  })
+                  .toFile(imgTmpFilePath);
+              } else if (imageOutputFormat === FileExtension.AVIF) {
+                await sharp(imgFilePath)
+                  .withMetadata()
+                  .avif({
+                    quality: imageOutputQuality,
+                  })
+                  .toFile(imgTmpFilePath);
+              }
+              let newImgFilePath = path.join(
+                imgFileFolderPath,
+                imgFileName + "." + imageOutputFormat
+              );
+              fs.unlinkSync(imgFilePath);
+              fileUtils.moveFile(imgTmpFilePath, newImgFilePath);
+              imgFilePaths[index] = newImgFilePath;
+            }
+          }
+        } catch (error) {
+          throw "Couldn't convert the images > " + error;
+        }
+      }
+      /////////////////////////////////
+      // bundle content
+      /////////////////////////////////
+      try {
+        console.log("\textracting to " + outputSubFolderPath);
+        if (comicInfoFilePath) imgFilePaths.push(comicInfoFilePath);
+        // create subFolderPath
+        if (!fs.existsSync(outputSubFolderPath)) {
+          fs.mkdirSync(outputSubFolderPath);
+          for (let index = 0; index < imgFilePaths.length; index++) {
+            let oldPath = imgFilePaths[index];
+            let newPath = path.join(
+              outputSubFolderPath,
+              path.basename(oldPath)
+            );
+            fileUtils.moveFile(oldPath, newPath);
+          }
+        } else {
+          throw "The output subfolder shouldn't exist " + outputSubFolderPath;
+        }
+      } catch (error) {
+        throw "Couldn't extract the file " + filePath + " > " + error;
+      }
+    } catch (error) {
+      fileUtils.cleanUpTempFolder();
+      console.log(
+        "ERROR: Couldn't extract the file to " +
+          outputSubFolderPath +
+          " > " +
+          error
+      );
+      summaryErrors++;
+      continue;
+    }
+    /////////////////////////////////
+    // clean up
+    /////////////////////////////////
+    summarySuccesses++;
+    fileUtils.cleanUpTempFolder();
+    console.log("Successfully extracted the file to " + outputSubFolderPath);
+  }
+  console.log("Extraction Finished:");
+  console.log("\ttotal files: " + summaryTotal);
+  console.log("\tfiles extracted: " + summarySuccesses);
+  console.log("\terrors: " + summaryErrors);
+}
+exports.start = start;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const { Command, Option } = require("commander");
 const convert = require("./convert");
+const extract = require("./extract");
 
 const program = new Command();
 program.name("acbt").version(require("../package.json").version);
+// CONVERT //////////////////////////////////////////////////////////////
 program
   .command("convert")
   .description("Convert and/or resize comic book files")
@@ -56,7 +58,6 @@ program
     "90"
   )
   // pdf
-  // TODO: pdf extraction method
   .addOption(
     new Option(
       "--pdf-creation-method <method>",
@@ -85,11 +86,76 @@ program
   
   Example calls:
     $ acbt convert comicbook.cb7
-    $ acbt convert -f cbz -p ./outputFolder comicbook.cb7
+    $ acbt convert -f cbz -o ./outputFolder comicbook.cb7
     $ acbt convert --format pdf comicbook_1.cbz ./comicbook_2.pdf
-    $ acbt convert --path ./outputFolder -f cb7 --scale 50 -if webp *.*`
+    $ acbt convert --output-folder ./outputFolder -f cb7 --scale 50 -if webp *.*`
   );
-
+// EXTRACT //////////////////////////////////////////////////////////////
+program
+  .command("extract")
+  .description("Extract comic book files' pages")
+  .argument(
+    "<input_file...>",
+    "input file/s; supported formats: cbz, cbr, cb7, epub (images only) & pdf"
+  )
+  // files
+  .option("-o, --output-folder <path>", "write the output to this folder", ".")
+  .addOption(
+    new Option(
+      "-co, --create-output-folder <bool>",
+      "create the output folder if the provided path doesn't exist"
+    )
+      .default("false")
+      .choices(["true", "false"])
+  )
+  .option(
+    "-s, --scale <integer>",
+    "scale the input file's pages by this percentage; must be an integer value between 1 and 100",
+    "100"
+  )
+  .option(
+    "-tp, --temp-path <path>",
+    "use this folder to write/read temporary files; the operating system's default directory for temporary files will be used if none is provided"
+  )
+  // images
+  .addOption(
+    new Option(
+      "-if, --image-format <format>",
+      "convert the input file's pages to this image format"
+    )
+      .default("original")
+      .choices(["original", "jpg", "png", "avif", "webp"])
+  )
+  .option(
+    "-iq, --image-quality <integer>",
+    "use this quality value when converting images; must be an integer value between 1 and 100",
+    "90"
+  )
+  // pdf
+  .addOption(
+    new Option(
+      "--pdf-extraction-method <method>",
+      "use this rendering method to set the dpi when extracting pdf image files"
+    )
+      .default("embedded")
+      .choices(["embedded", "render300", "render72"])
+  )
+  // action
+  .action(function (input_file, options, command) {
+    //console.log(options);
+    extract.start(input_file, options, command);
+  })
+  // help: example calls
+  .addHelpText(
+    "after",
+    `
+  
+  Example calls:
+    $ acbt extract comicbook.cb7
+    $ acbt extract -o ./outputFolder comicbook.cb7
+    $ acbt extract --output-folder ./outputFolder --scale 50 -if webp *.*`
+  );
+//////////////////////////////////////////////////////////////////////////////
 program.parse();
 
 // NOTE: to call from npm start add two dashes, ej: npm start convert -- --input ./al.cbz other.cbr -o something
